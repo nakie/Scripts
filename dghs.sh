5c333fa066c8290c73187207f6f148c81c8b4ab5
Nathan Crews <n_crews@hotmail.com>
2015-02-12 12:38:31 -0600

    update dghs script
    add info regarding pulling a single file from github repository.

--- dghs.sh
+++ dghs.sh
@@ -8,3 +8,8 @@
 
 echo "Getting Files from GitHub"
 curl -L https://github.com/nakie/Scripts/tarball/master | tar zx
+
+# Get single file form Scripts Repository.
+# wget https://github.com/nakie/Scripts/raw/master/file
+# chmod +x file
+
