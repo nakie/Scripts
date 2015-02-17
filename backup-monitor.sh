3d1fbcb358ff4c4d0628842636569b3ea7cfc01b
Nathan Crews <n_crews@hotmail.com>
2015-02-17 17:40:17 -0600

    backup
    Change completion test to use cpanel hook script

---
+++ backup-monitor.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+######################################################
+# ICS
+# Nathan Crews
+# backup-monitor.sh
+#
+# this script works along with backup-web.sh to automate the
+# the process of off loading copies of backups from the remote
+# web/email hosting server to an ICS local network server.
+#
+# backup-monitor.sh is hooked on to the end of the cpanel backup
+# process and updates a file when a backup has completed
+# 
+#
+# Get latest version from github
+# wget https://github.com/nakie/Scripts/raw/master/backup-monitor.sh
+# chmod +x backup-monitor.sh
+#
+# This script will be triggered using the cpanel hook system.
+# use cpanel's manage_hook to register the hook.
+# /usr/local/cpanel/bin/manage_hooks add script /root/backup-monitor.sh --category System --event Backup --stage post
+#
+#
+#######################################################
+
+    # Update access/modified time for the backup check file after cpbackup run finishes.
+touch -am /usr/local/ics/backup/lastrun
--- backup-web.sh
+++ backup-web.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 ######################################################
+# ICS
 # Nathan Crews
 # backup-web.sh
 #
 # Automate process for moving backups from remote 
 # server to local network for retention. 
 # 
-# Uses rsync to synchronize remote folder /backup/< daily backup name >
+# Uses rsync to synchronize the remote folder /backup/< daily backup name >
 # to Local network server in folder /server5-backup/
 #
+# Get latest version from github
+# wget https://github.com/nakie/Scripts/raw/master/backup-web.sh
+# chmod +x backup-web.sh
+#
 # The cron job that fires this is:
-# 0 5 * * 1-6 /usr/local/bin/ics/test/backup.sh 
+# 0 5 * * 1-6 /usr/local/bin/ics/backup-web.sh 
 # which starts the script at 5 am Sunday through Friday 
 # skipping a rename on the Saturday backup.
 #
@@ -22,7 +27,13 @@
 ##         CONFIGURATION / VARIABLES                  ##
 ########################################################
 
-    # Cpanel Backup Log File Location ( Remote Server ).
+    # Number of runs to try the backup
+ATTEMPTS=12
+
+    # Delay between attempts in minutes.
+MINUTES=30
+
+    # Location of backup ( Remote Server ).
 log_dir="/usr/local/cpanel/logs/cpbackup"
 
     # Cpbackup Backup destination location 
@@ -37,71 +48,46 @@ rmt_user="__USER__"
     # Domain/IP to remote server.
 rmt_server="__SERVER__"
 
-    # Flag to monitor backup state.
-FINNISHED=false
 
-for (( c=1; c<=12; c++ ))
+for (( c=1; c<=$ATTEMPTS; c++ ))
 do
-        # Retrieve most recent file in backup log directory 
-    file=$(ssh "$rmt_user"@"$rmt_server" ls "$log_dir" | sort -n | tail -1)
-     
-        # Check most recent log file to ensure it is from a
-        # backup today. -mmin -720 will confirm the file was
-        # Created in the past 12 hours.
-    if test `ssh "$rmt_user"@"$rmt_server" find "$log_dir"/"$file" -mmin -720` 
-    then  # Today's Log: continue
-
-            # Grab last 2 lines of Today's backup log file
-        output=$(ssh "$rmt_user"@"$rmt_server" tail -2 "$log_dir"/"$file")
-
-            # Check if Backup has completed.
-            # we'll find the "Completed" text if it has
-        if [[ $output =~ .*Completed.* ]]
-        then
-
-            # now that we know the backup is for today and has complted
-            # we will sync it to ICS's system.
-            # rsync -av -delete -e ssh $rmt_user@$rmt_server:$remote_dir $local_dir
-
-            echo "Backup Completed"
-
-                # once backup is synchronized exit loop
-            FINNISHED=true
-
-        else 
-            # There is a log for today
-            # but it does not appear to have finished yet.
-            FINNISHED=false
-        fi     
-
-    else  
-        # A Log for Today was not found.
-        FINNISHED=falses        
-    fi
     
-    
-    if $FINNISHED; 
+    # Confirm the Backup for today has completed.
+    if test `ssh "$rmt_user"@"$rmt_server" find "$log_dir"/"$file" -mmin -720`
     then
-    
+        # now that we know the backup is for today and has completed
+        # we will sync it to ICS's system.
+        # rsync -av -delete -e ssh $rmt_user@$rmt_server:$remote_dir $local_dir
+
+        echo "Backup Synchroniation Started"
+
+        
+        echo "Backup synchronization completed"
+            # once backup is synchronized exit loop
         break
-    else
+        
+    else # Backup has not finished.. now what?
     
-        sleep 30
+  
+        if [ $c!=12 ]
+        then
+        
+            #sleep $MINUTESm
+            
+            # Use seconds rather than minutes while testing
+            sleep $MINUTES
+            
+        else
+            
+            # on last loop send backup failure notification.
+            echo "Backup synchronization failed after 6 hours of trying"
+            
+        fi
         
     fi
     
 done
 
-
-if $FINNISHED; 
-then
-
-    echo "Backup synchronization completed"
-else
-
-    echo "Backup synchronization failed after 6 hours of trying"
-    
-fi
 # echo "Backup Copy Script Completed:"
 # echo $errmsg
 
