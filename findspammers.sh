80d46379b9af55bcd6405ec97506b304349bc5f8
Nathan Crews <n_crews@hotmail.com>
2015-08-27 14:26:31 -0500

    testing date formatting

--- findspammers.sh
+++ findspammers.sh
@@ -2,7 +2,10 @@
 ######################################################
 
 DAY=$(date +%Y-%m-%d)
-TIME=$(date +%H:%M| cut -c1-4)
-echo $DAY $TIME
+TIME=$(date +%H:%M | cut -c1-4 )
+
+MAILTIME="$DAY $TIME"
+
+echo $MAILTIME
 # Exim Date Example format - 2015-08-27 08:3
-#exigrep "" /var/log/exim_mainlog | exigrep "courier_login:" | perl -00ne '($from, $auth) = ( /\<\=\s(\S+).*?A\=courier\_login\:(\S+)/);if( uc($from) ne uc($auth) ){ print $from . " " . $auth ."\n" ; }'
+exigrep "$MAILTIME" /var/log/exim_mainlog | exigrep "courier_login:" | perl -00ne '($from, $auth) = ( /\<\=\s(\S+).*?A\=courier\_login\:(\S+)/);if( uc($from) ne uc($auth) ){ print $from . " " . $auth ."\n" ; }'
