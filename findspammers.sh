e445237451ba126e2c0f375fb3fb37aaaf6796a0
Nathan Crews <n_crews@hotmail.com>
2015-08-27 14:37:47 -0500

    update time

--- findspammers.sh
+++ findspammers.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 ######################################################
 
+# Create properly formatted date string
+# to match Exim's date format 
 DAY=$(date +%Y-%m-%d)
-TIME=$(date +%H:%M | cut -c1-4 )
-
+TIME=$(date --date="5 minutes ago" +%H:%M | cut -c1-4 )
 MAILTIME="$DAY $TIME"
 
-echo $MAILTIME
+#echo $MAILTIME
+
 # Exim Date Example format - 2015-08-27 08:3
 exigrep "$MAILTIME" /var/log/exim_mainlog | exigrep "courier_login:" | perl -00ne '($from, $auth) = ( /\<\=\s(\S+).*?A\=courier\_login\:(\S+)/);if( uc($from) ne uc($auth) ){ print $from . " " . $auth ."\n" ; }'
