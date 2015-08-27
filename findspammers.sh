7c6103dd254d32e17741365de4f98262231ed20d
Nathan Crews <n_crews@hotmail.com>
2015-08-27 16:41:37 -0500

    updating find spammers to send email

--- findspammers.sh
+++ findspammers.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 ######################################################
 
+#the email address to send the warning
+EMAIL="ncrews@chooseics.com"
+
 # Create properly formatted date string
-# to match Exim's date format 
+# to match Exim's date format
 DAY=$(date +%Y-%m-%d)
 TIME=$(date --date="5 minutes ago" +%H:%M | cut -c1-4 )
-MAILTIME="$DAY $TIME"
+MAILTIME="$DAY"
 
-#echo $MAILTIME
+echo $MAILTIME
 
 # Exim Date Example format - 2015-08-27 08:3
-exigrep "$MAILTIME" /var/log/exim_mainlog | exigrep "courier_login:" | perl -00ne '($from, $auth) = ( /\<\=\s(\S+).*?A\=courier\_login\:(\S+)/);if( uc($from) ne uc($auth) ){ print $from . " " . $auth ."\n" ; }'
+
+LOGSTOREVIEW=$(exigrep "$MAILTIME" /var/log/exim_mainlog | exigrep "courier_login:" | perl -00ne '($from, $auth) = ( /\<\=\s(\S+).*?A\=courier\_login\:(\S+)/);if( uc($from) ne uc($auth) ){ print $from . " " . $auth . "\n" ; }')
+
+if [ -n "$LOGSTOREVIEW" ];
+then
+
+        echo ${LOGSTOREVIEW} | /bin/mail -s "Exim sender/Auth mismatch found..." ${EMAIL}
+
+fi
